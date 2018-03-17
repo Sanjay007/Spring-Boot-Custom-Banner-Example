9ab5fb66e64c14c203ef33b441ede3ea14dce177
Sanjay <bqangarsanju12@gmail.com>
2018-03-17 19:12:32 +0530

    kk

---
+++ src/main/java/com/frugalis/SpringBootCustomBannerExample/SpringBootCustomBannerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.frugalis.SpringBootCustomBannerExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomBannerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomBannerExampleApplication.class, args);
+	}
+}
